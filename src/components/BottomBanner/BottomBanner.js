9ada94eb9b4a5e9253e28b39810737db373dc2d2
Janickvw <janick.van.well@gmail.com>
2018-08-24 19:44:37 +1200

    Updated to include back end support

---
+++ src/components/BottomBanner/BottomBanner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const BottomBanner = () => {
+	return (
+		<div style= {{}}>
+			<a
+			href='https://github.com/Janickvw'
+			target='_blank'
+			rel='noopener noreferrer'
+			className='f6 link dim white underline pa3 pointer fixed bottom-0 right-0'
+			>Made by Janick van Well</a>
+		</div>
+	);
+}
+
+export default BottomBanner;
